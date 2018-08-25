c2e2a84ea97a1e6794d743ccd14c25754c6bf595
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-24 22:12:52 -0300

    Removing sass configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import './assets/CSS_SCSS/header.css';
 
 class App extends Component {
+
+  state = {
+    members: [
+      "Bernardo", "Clarissa", "Ésio", "Felipe",
+      "Jacó", "Lucas", "Mariana", "Pedro", "Saleh", "Youssef"
+    ],
+  }
+
   render() {
     return (
-      <div className="App">
-        <p className="teste">Olár</p>
+      // <SideNav />
+      // <Header />
+      // <Resume />
+      <div>
+        <h1 className="title">EAEA</h1>
       </div>
-    );
+    )
+  }
+
+  renderMembers = () => {
+    return 
   }
 }
 
