1d5c9d7470128c4520e888634828b396b05e67da
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-24 21:26:42 -0300

    Project's structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './testando.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="teste">Olár</p>
-        <h1 className="titulo">EAAAAAAE</h1>
       </div>
     );
   }
