faaf1155cc961498ce606b4e2130e7144d25f610
Mariana Pícolo <marianapicolo@hotmail.com>
2018-10-07 19:44:52 -0300

    Add document of sprint 7 planning

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -42,7 +42,7 @@ const siteConfig = {
     { doc: 'tap', label: 'Produto' },
     { doc: 'overview', label: 'Projeto' },
     { doc: 'psprint0', label: 'Release I' },
-    // { doc: 'psprint0', label: 'Release II' },
+    { doc: 'psprint7', label: 'Release II' },
     { blog: false },
   ],
 
